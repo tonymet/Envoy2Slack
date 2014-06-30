a72dc17b95a3176a90c9089f5d93adab96ad38e9
Harper Reed <harper@nata2.org>
2014-06-30 13:24:34 -0500

    initial import

---
+++ README.md
@@ -0,0 +1,54 @@
+#Envoy 2 Slack
+
+A node app that proxies visitor notifications from [Envoy](https://signwithenvoy.com) to [Slack](http://slack.com)
+
+## How to get it going:
+
+###Install
+
+Check it out of git:
+
+	git clone https://github.com/modestinc/Envoy2Slack.git
+
+Deploy to heroku:
+
+	heroku create
+	git push heroku master
+
+This will tell you your url: `https://creative-name-1234.herokuapp.com`
+
+I imagine you could host it yourself, but why?
+
+###Config
+
+####Environmental Vars
+
+You will need to set the following environmental variables:
+
+	SLACK_BOTNAME:      buildbot
+	SLACK_CHANNEL:      #code
+	SLACK_ORGANIZATION: lunar
+	SLACK_TOKEN:        xxxx
+
+You can do this the easy way with heroku:
+
+	heroku config:add SLACK_BOTNAME=OfficeBot
+	heroku config:add SLACK_CHANNEL=#office
+	heroku config:add SLACK_ORGANIZATION=modest
+	heroku config:add SLACK_TOKEN=xxx
+
+
+###That should do it.
+
+
+##Todo
+
+
+There probably needs to be some better security on the url that is posted to the webhooks. Maybe pass through the `token` that comes from slack via the `circle.yml` config.
+
+Probably other things as well.
+
+
+##Yay.
+
+hit me up if you have problems or concerns: [@harper](http://twitter.com/harper) / [harper@nata2.org](mailto:harper@nata2.org)
---
+++ app.js
@@ -0,0 +1,96 @@
+require('newrelic');
+
+var dateFormat = require('dateformat');
+var express = require("express");
+var qs = require('querystring');
+var requests = require('request');
+
+var app = express();
+app.use(express.logger());
+app.use(express.bodyParser());
+
+app.get('/', function(request, response) {
+    response.send('Hi! Go Away!');
+});
+
+String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g, '');};
+
+/* Handle incoming posts from circleci */
+post_handler = function(payload) {
+    console.log(payload);
+    visitor = JSON.parse(payload['entry']);
+
+    console.log(visitor);
+    console.log(visitor["id"]);
+    console.log(visitor['your_full_name']);
+    var date = new Date(visitor['signed_in_time_local']);
+    console.log(date);
+    signin_date = dateFormat(date, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+    console.log(signin_date);
+    console.log(visitor['who_are_you_here_to_see\?']);
+
+
+    /* This is the message. tweak it to make it better */
+
+    message_string = visitor['your_full_name']+" is here to see "+visitor['who_are_you_here_to_see\?']+".  <" + visitor['photo_url'] + "| Picture of "+visitor['your_full_name']+">"
+
+
+    slack_org = process.env.SLACK_ORGANIZATION;
+    slack_token = process.env.SLACK_TOKEN;
+    slack_channel = process.env.SLACK_CHANNEL;
+    slack_botname = process.env.SLACK_BOTNAME;
+
+    slack_url = "https://" + slack_org + ".slack.com/services/hooks/incoming-webhook?token=" + slack_token;
+
+    slack_payload = {
+        "text": message_string,
+        "channel" : slack_channel,
+        "username" : slack_botname,
+        "icon_emoji": ":ghost:"
+    };
+
+    /* Post to slack! */
+    console.log(slack_payload);
+    requests.post(slack_url, {json:slack_payload},
+    function (error, response, body) {
+        console.log(body);
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        }
+    });
+};
+
+app.get('/hook/', function(request, response) {
+    response.redirect('/');
+});
+
+app.post('/hook/', function(request, response) {
+
+    console.log("Got response: " + response.statusCode);
+    response.send("Thank you!");
+    post_handler(request.body);
+
+});
+
+/*
+There has to be a better way to do this. Thoughts?
+*/
+
+if ((typeof process.env.SLACK_BOTNAME !== 'undefined' && process.env.SLACK_BOTNAME)||
+    (typeof process.env.SLACK_CHANNEL !== 'undefined' && process.env.SLACK_CHANNEL)||
+    (typeof process.env.SLACK_ORGANIZATION !== 'undefined' && process.env.SLACK_ORGANIZATION)||
+    (typeof process.env.SLACK_TOKEN !== 'undefined' && process.env.SLACK_TOKEN)
+    )
+{
+    var port = process.env.PORT || 5000;
+    app.listen(port, function() {
+        console.log("Listening on " + port);
+    });
+}else{
+    console.log("One of the required config variables missing:");
+    console.log("\tSLACK_BOTNAME: " + process.env.SLACK_BOTNAME);
+    console.log("\tSLACK_CHANNEL: " + process.env.SLACK_CHANNEL);
+    console.log("\tSLACK_ORGANIZATION: " + process.env.SLACK_ORGANIZATION);
+    console.log("\tSLACK_TOKEN: " + process.env.SLACK_TOKEN);
+    process.exit();
+}
